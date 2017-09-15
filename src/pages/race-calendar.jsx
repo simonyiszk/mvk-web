901f169324a07bf4ef89a6f9195c8bf4b6c97078
Kristóf Poduszló <kripod@protonmail.com>
2017-09-16 01:28:43 +0200

    Fix appearance of the race calendar page

--- src/pages/race-calendar.jsx
+++ src/pages/race-calendar.jsx
@@ -2,9 +2,28 @@ import moment from 'moment';
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import Helmet from 'react-helmet';
+import ArticleContainer from '../components/article-container';
+import CoverImage from '../components/cover-image';
 
 BigCalendar.momentLocalizer(moment);
 
-const RaceCalendar = () => <BigCalendar events={[]} />;
+const RaceCalendar = () => {
+  const title = 'Versenynaptár';
+
+  return (
+    <div>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <CoverImage />
+
+      <ArticleContainer title={title}>
+        <BigCalendar events={[]} />
+      </ArticleContainer>
+    </div>
+  );
+};
 
 export default RaceCalendar;
