bcaaaf39079abdd263d5e5ec1e5c10c4f5c5a0ba
Kristóf Poduszló <kripod@protonmail.com>
2017-09-16 02:30:52 +0200

    Improve cover image placeholder and the legibility of cover-overlapping article headlines

--- src/components/cover-image.jsx
+++ src/components/cover-image.jsx
@@ -1,14 +1,17 @@
 import { css } from 'glamor';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { ASPECT_RATIO_21_9_RELATIVE_HEIGHT, headerBackground } from '../utils/presets';
+import { ASPECT_RATIO_21_9_RELATIVE_HEIGHT } from '../utils/presets';
 
 const CoverImage = ({ src, ...props }) => (
   <div
     {...css({
-      background: headerBackground,
+      background: 'linear-gradient(137.51deg, rgba(39, 170, 225, 0.7), rgba(227, 210, 0, 0.6))',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
       height: `${ASPECT_RATIO_21_9_RELATIVE_HEIGHT}vw`,
       marginBottom: '-10rem',
+      position: 'relative',
     })}
   >
     {src != null && (
@@ -18,11 +21,20 @@ const CoverImage = ({ src, ...props }) => (
         {...css({
           height: '100%',
           objectFit: 'cover',
+          position: 'absolute',
           width: '100%',
         })}
         {...props}
       />
     )}
+
+    <div
+      {...css({
+        background: 'linear-gradient(transparent 62%, rgba(0, 0, 0, 0.62))',
+        height: '100%',
+        position: 'relative',
+      })}
+    />
   </div>
 );
 
--- src/utils/presets.js
+++ src/utils/presets.js
@@ -3,8 +3,6 @@ export const ASPECT_RATIO_4_3 = { paddingBottom: '75%' };
 export const ASPECT_RATIO_21_9_RELATIVE_HEIGHT = 42.86;
 export const ASPECT_RATIO_21_9 = { paddingBottom: `${ASPECT_RATIO_21_9_RELATIVE_HEIGHT}%` };
 
-export const headerBackground = '#2196f3'; // Material Design blue 500
-
 export const mainBackground = '#efefef';
 export const mainLinkColor = '#2196f3'; // Material Design blue 500, old: '#25aae1'
 
