ae4ab61fd2b613d9dde3a313d4fe11595ec74ac3
Kristóf Poduszló <kripod@protonmail.com>
2017-11-07 00:19:15 +0100

    Add experimental Gallery menu

---
+++ src/pages/gallery.jsx
@@ -0,0 +1,108 @@
+import Link from 'gatsby-link';
+import { css } from 'glamor';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Helmet from 'react-helmet';
+import ArticleContainer from '../components/article-container';
+import CoverImage from '../components/cover-image';
+import { ASPECT_RATIO_4_3, IMAGE_OVERLAY_TINT } from '../utils/presets';
+
+const GalleryPage = ({ data }) => {
+  const title = 'Galéria';
+  const posts = data.allMarkdownRemark;
+
+  return (
+    <div>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <CoverImage />
+
+      <ArticleContainer title={title}>
+        {/* TODO: Abstract the logic below into a new component */}
+        <Grid container>
+          {posts.edges.map(({ node: post }) => {
+            const image =
+              post.frontmatter.image && post.frontmatter.image.childImageSharp.resolutions;
+
+            return (
+              <Grid item xs={12} sm={6} md={4} key={post.fields.slug}>
+                <Link to={post.fields.slug} {...css({ textDecoration: 'none' })}>
+                  <Card {...css({ height: '100%' })}>
+                    <div
+                      {...css({
+                        ...ASPECT_RATIO_4_3,
+                        background: IMAGE_OVERLAY_TINT,
+                        position: 'relative',
+                      })}
+                    >
+                      {image && (
+                        <CardMedia
+                          component="img"
+                          {...image}
+                          {...css({
+                            height: '100%',
+                            objectFit: 'cover',
+                            position: 'absolute',
+                          })}
+                        />
+                      )}
+                    </div>
+                    <CardContent>
+                      <Typography type="title">{post.frontmatter.title}</Typography>
+                      <Typography color="secondary">{post.frontmatter.date}</Typography>
+                      <Typography>
+                        {post.frontmatter.excerpt != null ? post.frontmatter.excerpt : post.excerpt}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </ArticleContainer>
+    </div>
+  );
+};
+
+GalleryPage.propTypes = {
+  data: PropTypes.shape({}).isRequired,
+};
+
+export default GalleryPage;
+
+export const query = graphql`
+  query GalleryPageQuery {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/gallery/" }, frontmatter: { draft: { ne: true } } }
+      sort: { fields: [frontmatter___date, frontmatter___title], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt(pruneLength: 80)
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+            excerpt
+            image {
+              childImageSharp {
+                resolutions(width: 540, height: 405) {
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
