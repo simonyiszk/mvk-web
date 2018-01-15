42d4d7d0456d2237421dde8e102716354c67711f
Kristóf Poduszló <kripod@protonmail.com>
2018-01-15 22:45:38 +0100

    Make sponsors editable through CMS

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -2,8 +2,7 @@ import { css } from 'glamor';
 import Paper from 'material-ui/Paper';
 import React from 'react';
 import Helmet from 'react-helmet';
-import BMELogoImage from '../assets/bme-logo.svg';
-import EHKLogoImage from '../assets/ehk-logo.svg';
+import { sponsors } from '../data/sponsors.json';
 import ArticleContainer from '../components/article-container';
 import CoverImage from '../components/cover-image';
 
@@ -28,13 +27,19 @@ const SponsorsPage = () => {
               margin: '-1rem',
 
               '& > *': {
-                height: '6rem',
-                padding: '1rem',
+                margin: '1rem',
               },
             })}
           >
-            <img src={BMELogoImage} alt="Műegyetem 1782" />
-            <img src={EHKLogoImage} alt="Egyetemi Hallgatói Képviselet" />
+            {sponsors.map(sponsor => (
+              <a href={sponsor.website} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={sponsor.logo}
+                  alt={sponsor.name}
+                  {...css({ height: `${sponsor.importance}rem` })}
+                />
+              </a>
+            ))}
           </div>
         </Paper>
       </ArticleContainer>
