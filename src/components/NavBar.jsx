e0c91365b3215d660916a400ecd89e60a322b374
Kristóf Poduszló <kripod@protonmail.com>
2018-05-18 23:31:42 +0200

    Make NavBar transparent by default

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,51 +2,103 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Link, StaticQuery } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
 import MVKLogoWhiteURL from '../data/assets/mvk-logo-white.svg';
 import styles from './NavBar.module.scss';
 
-const NavBar = ({ ...props }) => (
-  <AppBar {...props}>
-    <Toolbar>
-      <div className={styles.brandContainer}>
-        <Link to="/">
-          <img
-            src={MVKLogoWhiteURL}
-            alt="Kezdőlap"
-            className={styles.brandLogo}
-          />
-        </Link>
-      </div>
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowScrollY: 0,
+    };
 
-      <StaticQuery
-        query={graphql`
-          query NavBarQuery {
-            allNavbarItemsYaml {
-              edges {
-                node {
-                  label
-                  url
+    this.handleScroll = this.handleScroll.bind(this);
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  componentWillUnmount() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  handleScroll() {
+    this.setState({
+      windowScrollY: window.scrollY,
+    });
+  }
+
+  render() {
+    const { style, ...props } = this.props;
+    const { windowScrollY } = this.state;
+
+    return (
+      <AppBar
+        elevation={windowScrollY === 0 ? 0 : undefined}
+        style={{
+          transition: 'all 0.5s ease-out',
+          ...(windowScrollY === 0
+            ? {
+                background: 'transparent',
+                textShadow: '0 0 0.75em rgba(0, 0, 0, 0.8)',
+              }
+            : {}),
+          ...style,
+        }}
+        {...props}
+      >
+        <Toolbar>
+          <div className={styles.brandContainer}>
+            <Link to="/">
+              <img
+                src={MVKLogoWhiteURL}
+                alt="Kezdőlap"
+                className={styles.brandLogo}
+              />
+            </Link>
+          </div>
+
+          <StaticQuery
+            query={graphql`
+              query NavBarQuery {
+                allNavbarItemsYaml {
+                  edges {
+                    node {
+                      label
+                      url
+                    }
+                  }
                 }
               }
+            `}
+            render={staticData =>
+              staticData.allNavbarItemsYaml.edges.map(({ node }) => (
+                <Button
+                  key={node.url}
+                  component={Link}
+                  to={node.url}
+                  color="inherit"
+                >
+                  {node.label}
+                </Button>
+              ))
             }
-          }
-        `}
-        render={staticData =>
-          staticData.allNavbarItemsYaml.edges.map(({ node }) => (
-            <Button
-              key={node.url}
-              component={Link}
-              to={node.url}
-              color="inherit"
-            >
-              {node.label}
-            </Button>
-          ))
-        }
-      />
-    </Toolbar>
-  </AppBar>
-);
+          />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
 
-export default NavBar;
+NavBar.propTypes = {
+  style: PropTypes.shape(),
+};
+
+NavBar.defaultProps = {
+  style: {},
+};
